e1b9b72f501f22e350d0a78853d1858fcd01d3a7
joao-hideki <joaoohideki12345@gmail.com>
2022-11-27 01:55:53 -0300

    finalizando a página home

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+  Card, Container, Header, ListContainer,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export function ContactsList() {
   return (
@@ -7,6 +12,68 @@ export function ContactsList() {
         <strong>N contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="top arrow" />
+          </button>
+        </header>
+        <Card>
+          <div className="info">
+            <div>
+              <strong>João Hideki</strong>
+              <small>Instagram</small>
+            </div>
+            <span>joao@email.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div>
+              <strong>João Hideki</strong>
+              <small>Instagram</small>
+            </div>
+            <span>joao@email.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div>
+              <strong>João Hideki</strong>
+              <small>Instagram</small>
+            </div>
+            <span>joao@email.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
