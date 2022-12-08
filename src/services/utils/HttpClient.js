09cb6f26c5248e72f99f58e311a335570bbff0a2
joao-hideki <joaoohideki12345@gmail.com>
2022-12-08 01:18:08 -0300

    tratando os erros retornados pela API e centralizando o tratamento deles

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -6,9 +6,12 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseUrl}${path}`);
     await delay(500);
-    return response.json();
+    const response = await fetch(`${this.baseUrl}${path}`);
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error();
   }
 }
 
